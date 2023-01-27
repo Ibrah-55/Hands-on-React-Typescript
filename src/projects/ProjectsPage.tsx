e6483473b027c9353d469664d041c968c8c906a0
Ibrah <ibrah@pop-os.localdomain>
2023-01-27 11:22:45 +0300

    Passing data to a component

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { MOCK_PROJECTS } from './MockProject'
+import ProjectList from './ProjectList';
 
 export default function ProjectsPage() {
     return (
         <>
         <h4>Projects</h4>
-      <pre>{JSON.stringify(MOCK_PROJECTS, null, '')}</pre> 
+      <ProjectList projects={MOCK_PROJECTS} />
       </>
       
   );
