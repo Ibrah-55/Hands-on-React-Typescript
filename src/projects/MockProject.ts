051152b9a685feea10ad5cb0d54e38b89bad4748
Ibrah <ibrah@pop-os.localdomain>
2023-01-27 11:40:18 +0300

    Displaying List Data

--- src/projects/MockProject.ts
+++ src/projects/MockProject.ts
@@ -3,9 +3,9 @@ import { Project } from './Project';
 export const MOCK_PROJECTS = [
   new Project({
     id: 1,
-    name: 'John - Genesis',
+    name: 'Morroco',
     description:
-      'Configured Svlte project',
+      'Magreb region of North Africa',
     imageUrl: '/assets/placeimg_500_300_arch4.jpg',
     contractTypeId: 3,
     contractSignedOn: '2020-08-04T22:39:41.473Z',
@@ -14,9 +14,9 @@ export const MOCK_PROJECTS = [
   }),
   new Project({
     id: 2,
-    name: 'Django Group',
+    name: 'France',
     description:
-      'Centralized Django Models',
+      'Western Europe',
     imageUrl: '/assets/placeimg_500_300_arch1.jpg',
     contractTypeId: 4,
     contractSignedOn: '2020-08-06T21:21:31.419Z',
@@ -25,9 +25,9 @@ export const MOCK_PROJECTS = [
   }),
   new Project({
     id: 3,
-    name: 'React Login',
+    name: 'Mexico',
     description:
-      'Jwt auth',
+      'County in North America',
     imageUrl: '/assets/placeimg_500_300_arch12.jpg',
     contractTypeId: 6,
     contractSignedOn: '2021-06-26T18:24:01.706Z',
@@ -36,9 +36,9 @@ export const MOCK_PROJECTS = [
   }),
   new Project({
     id: 4,
-    name: 'Typescript',
+    name: 'South Korea',
     description:
-      'Javasript with Types',
+      'East Asia',
     imageUrl: '/assets/placeimg_500_300_arch5.jpg',
     contractTypeId: 4,
     contractSignedOn: '2021-05-26T01:10:42.344Z',
@@ -47,10 +47,10 @@ export const MOCK_PROJECTS = [
   }),
   new Project({
     id: 5,
-    name: 'Gatsby',
+    name: 'Kenya',
     description:
-      'Managed logistical migration.',
-    imageUrl: '/assets/placeimg_500_300_arch12.jpg',
+      'East Africa',
+    imageUrl: '/assets/placeimg_500_300_arch10.jpg',
     contractTypeId: 2,
     contractSignedOn: '2021-12-18T21:46:47.944Z',
     budget: 81188,
@@ -58,9 +58,10 @@ export const MOCK_PROJECTS = [
   }),
   new Project({
     id: 6,
-    name: 'Tailwind',
+    name: 'Egypt',
     description:
-    'scanning all of your HTML files, JavaScript components, and any other templates for class names,generating the corresponding styles and then writing them to a static CSS file.',
+    'North Africa',
+    imageUrl: '/assets/placeimg_500_300_arch7.jpg',
     contractTypeId: 3,
     contractSignedOn: '2016-09-23T21:27:25.035Z',
     budget: 53407,
