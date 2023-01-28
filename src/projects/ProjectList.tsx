1b0a6218e92f674caf709d5e83d7e20860823815
Ibrah <ibrah@pop-os.localdomain>
2023-01-28 08:34:19 +0300

    Created Form to edit data

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Project} from './Project';
 import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
 
 interface ProjectListProps{
     projects: Project[];
@@ -10,6 +11,8 @@ function ProjectList({projects}: ProjectListProps) {
          const items = projects.map((project) => (
                <div key={project.id} className="cols-sm">
                  <ProjectCard project={project}></ProjectCard>
+                 < ProjectForm />
+
                </div>
              ))
              return <div className='row'>{items}</div>
